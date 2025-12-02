5bbbcba51412b3066e1d7d1b1d024386d788fba9
yasinquluzada <yasinquluzad850@gmail.com>
2025-12-02 12:02:54 +0000

    Add script 2-args.py for python-import_modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 from sys import argv
+
+
 def main():
+    """Print the number of and list of arguments."""
     count = len(argv) - 1
+
     if count == 0:
-        print("0  arguments.")
-        
+        print("0 arguments.")
     elif count == 1:
-        print("1: argument")
+        print("1 argument:")
     else:
         print("{} arguments:".format(count))
-    for i in range(2, count + 1):
+
+    for i in range(1, count + 1):
         print("{}: {}".format(i, argv[i]))
-        
+
+
 if __name__ == "__main__":
     main()
