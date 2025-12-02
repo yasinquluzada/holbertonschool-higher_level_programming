c5aac96ae0a43841a932f6074f0405f2557ff823
yasinquluzada <yasinquluzad850@gmail.com>
2025-12-02 13:40:10 +0000

    Add script 0-print_list_integer.py

--- python-data_structures/0-main.py
+++ python-data_structures/0-main.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Module that defines print_list_integer function."""
+
+
 def print_list_integer(my_list=[]):
+    """Prints all integers of a list, one per line.
+
+    Args:
+        my_list (list): List of integers.
+    """
     for number in my_list:
-        print("{%d}".format(number))
-        
+        print("{:d}".format(number))
