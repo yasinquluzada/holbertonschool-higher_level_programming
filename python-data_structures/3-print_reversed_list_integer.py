6435fcaf3971b57f1cf5c092fea48808ae4e2413
yasinquluzada <yasinquluzad850@gmail.com>
2025-12-05 09:14:43 +0000

    Fix trailing whitespace and finalize reverse print function

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""Module that defines print_reversed_list_integer."""
+
+
 def print_reversed_list_integer(my_list=[]):
+    """Print all integers of a list in reverse order.
+
+    Args:
+        my_list (list): list of integers.
+    """
     if my_list is None:
-        return 
-    for i in reversed(my_list):
-        print("{:d}".format(i))
+        return
+
+    for value in reversed(my_list):
+        print("{:d}".format(value))
