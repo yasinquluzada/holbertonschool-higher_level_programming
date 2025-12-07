c8a5ef03f2623202428d066580d2b70169f52df2
yasinquluzada <yasinquluzad850@gmail.com>
2025-12-07 19:45:21 +0000

    Add script 4-main.py

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        result = 0
+        try:
+            a = my_list_1[i]
+            b = my_list_2[i]
+            if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+                print("wrong type")
+                continue
+            result = a / b
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            new_list.append(result)
+    return new_list
