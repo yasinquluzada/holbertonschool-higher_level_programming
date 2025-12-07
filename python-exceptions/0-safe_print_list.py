56f3f715e036b8c953a88d026a34a0c9c62f69fc
yasinquluzada <yasinquluzad850@gmail.com>
2025-12-07 19:39:55 +0000

    Add script 0-main.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""Module that defines safe_print_list function."""
+
+
 def safe_print_list(my_list=[], x=0):
-    
+    """Prints x elements of a list.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): Number of elements to attempt to print.
+
+    Returns:
+        int: The real number of elements printed.
+    """
     count = 0
 
     for i in range(x):
