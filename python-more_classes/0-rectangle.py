78a5dba2ace7cb47e9f085e870a911d5e6031dfc
yasinquluzada <yasinquluzad850@gmail.com>
2025-12-14 19:30:12 +0000

    Add empty Rectangle class

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""This module defines a basic Rectangle class as a starting point for more features."""
+"""
+This module defines a basic Rectangle class.
+It is a starting point for more features.
+"""
 
 
 class Rectangle:
