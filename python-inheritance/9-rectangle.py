10b47de924209cf24f6e858613c7f0f7115137a2
yasinquluzada <yasinquluzad850@gmail.com>
2025-12-21 17:07:14 +0000

    Add Rectangle inheriting from BaseGeometry with area and __str__

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Defines a Rectangle with area computation and string representation."""
-
+"""Defines a Rectangle class based on BaseGeometry validation and behavior."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represents a rectangle defined by validated width and height."""
+    """Represents a rectangle validated by BaseGeometry integer_validator."""
 
     def __init__(self, width, height):
-        """Initializes the rectangle with validated width and height."""
+        """Initializes a Rectangle with validated positive integer width and height."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
@@ -19,5 +18,5 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        """Returns the printable description of the rectangle."""
+        """Returns the string representation of the rectangle."""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
