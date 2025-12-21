e64e6f7ad6bb1647074817c04d5e8f151677dc55
yasinquluzada <yasinquluzad850@gmail.com>
2025-12-21 18:21:21 +0000

    SQL_more_queries: fix user_0d_1 privileges and password

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
--- Task 1: Create MySQL user user_0d_1 with all privileges on the server.
+-- Task 1: Create MySQL user user_0d_1 with all privileges on the server and set password.
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
