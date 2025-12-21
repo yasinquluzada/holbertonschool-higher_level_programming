dc9a1c779e525962330c7bba02e7039441b626e8
yasinquluzada <yasinquluzad850@gmail.com>
2025-12-21 18:19:29 +0000

    SQL_more_queries: create root-like user user_0d_1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Task 1: Create MySQL user user_0d_1 with all privileges on the server.
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
