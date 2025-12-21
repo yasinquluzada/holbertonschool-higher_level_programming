200b9bccec1fd056564bd4c4d9f5177f64d99531
yasinquluzada <yasinquluzad850@gmail.com>
2025-12-21 18:24:49 +0000

    SQL_more_queries: create hbtn_0d_usa and states table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,14 @@
+-- Task 6: Create database hbtn_0d_usa and table states with required fields.
+
+-- Create database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Select the database
+USE hbtn_0d_usa;
+
+-- Create table states if it does not exist
+CREATE TABLE IF NOT EXISTS states (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id)
+);
