1bfccef07b4c6107adbbb5a9946bf49b3d3b666e
yasinquluzada <yasinquluzad850@gmail.com>
2025-12-21 18:31:43 +0000

    SQL_more_queries: list shows with no genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,9 @@
+-- Task 12: List all shows in hbtn_0d_tvshows without a genre linked.
+
+-- Select shows with no linked genre
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
